2c9eceed0188265b704ea60bd8402a6fe410b49b
Nick Matyushin <n.matyushin@comagic.dev>
2025-08-18 18:01:42 +0300

    chapter 12

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import { lusitana } from "./ui/fonts";
 import Image from "next/image";
 
-export default function Page() {
+export default async function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
